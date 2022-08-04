f9a4333ee2724b133c8da68de25e023dba50d387
brentnequin <brentnequin@gmail.com>
2022-08-03 22:16:00 -0500

    more aligning

--- client/components/TheHeader.vue
+++ client/components/TheHeader.vue
@@ -1,9 +1,9 @@
 <template>
     <header>
         <b-navbar toggleable="lg" type="dark" variant="secondary">
-            <b-navbar-brand to="/" class="brand">
-                <img fluid src="~/static/favicon.svg" class="d-inline-block align-center logo" alt="logo">
-                VESE
+            <b-navbar-brand to="/" class="brand d-flex justify-content-center ml-3">
+                <img fluid src="~/static/favicon.svg" class="d-inline-block mr-2 align-center logo" alt="logo">
+                <p class="mb-0 align-self-center">VESE</p>
             </b-navbar-brand>
             <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
             <b-collapse id="nav-collapse" is-nav>
