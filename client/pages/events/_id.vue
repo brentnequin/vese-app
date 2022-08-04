2617b0839c8b33fd20d9b3738cb84b04d6d0380f
brentnequin <brentnequin@gmail.com>
2022-08-03 22:52:08 -0500

    event page per id

---
+++ client/pages/events/_id.vue
@@ -0,0 +1,35 @@
+<template>
+    <page>
+        <b-container class="my-5" slot="content">
+            <b-media>
+                <template #aside>
+                    <b-img
+                        src="https://cdn.vox-cdn.com/thumbor/jW1i6Uqp0JwSMvDFHmF2xWmJU6g=/1400x1400/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/9186447/lou_malnati_s.0.jpg"
+                        width="300" alt="placeholder" class="mr-3">
+                    </b-img>
+                </template>
+                <h1>{{ event.title }}</h1>
+                <p class="text-muted">{{ event.starttime }} - {{ event.endtime }} | {{ event.date }}</p>
+                <p>{{ event.fulldescription}}</p>
+            </b-media>
+        </b-container>
+    </page>
+</template>
+
+<script>
+export default {
+    props: ['id'],
+    data() {
+        return {
+            event: {
+                title: "It's Pizza Time",
+                date: "Aug 3, 2022",
+                starttime: "8:00am",
+                endtime: "9:00am",
+                excerpt: "It's Pizza Time It's Pizza Time It's Pizza Time It's Pizza Time It's Pizza Time",
+                fulldescription: "Lou Malnati's Pizzeria is an American Chicago-style pizza restaurant chain headquartered in Northbrook, Illinois. It was founded by the son of Rudy Malnati, who was instrumental in developing the recipe for Chicago-style pizza, and it has become one of the Chicago area's best-known local lines of pizza restaurants."
+            }
+        }
+    }
+}
+</script>
--- client/pages/events/index.vue
+++ client/pages/events/index.vue
@@ -7,16 +7,23 @@
       </b-col>
       <b-col class="mt-5">
         <ul class="list-unstyled">
-          <b-media class="mb-2" tag="li" v-for="(event, index) in eventsLoaded" v-bind:key="index">
+          <b-media class="mb-4" tag="li" v-for="(event, index) in eventsLoaded" v-bind:key="index">
             <template #aside>
-              <b-img src="https://cdn.vox-cdn.com/thumbor/jW1i6Uqp0JwSMvDFHmF2xWmJU6g=/1400x1400/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/9186447/lou_malnati_s.0.jpg" width="128" alt="placeholder"></b-img>
+              <b-link :to="'events/' + event.id">
+                <b-img src="https://cdn.vox-cdn.com/thumbor/jW1i6Uqp0JwSMvDFHmF2xWmJU6g=/1400x1400/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/9186447/lou_malnati_s.0.jpg" width="150" alt="placeholder"></b-img>
+              </b-link>
             </template>
-            <h5 class="mt-0 mb-1">{{ event.title }}</h5>
+            <b-link :to="'events/' + event.id">
+              <h5 class="mt-0 mb-1">{{ event.title }}</h5>
+            </b-link>
             <p class="text-muted">{{ event.starttime }} - {{ event.endtime }} | {{ event.date }}</p>
             <p class="mb-0">{{ event.description }}</p>
+            <b-button class="mt-3" :to="'events/' + event.id">Read More</b-button>
           </b-media>
         </ul>
-        <button v-if="!allEventsLoaded" v-on:click="loadMoreEvents">View More</button>
+        <div class="d-flex justify-content-center">
+          <b-button variant="primary" v-if="!allEventsLoaded" v-on:click="loadMoreEvents">View More</b-button>
+        </div>
       </b-col>
     </b-row>
   </b-container>
@@ -45,42 +52,48 @@ export default {
           "date": "Aug 3, 2022",
           "starttime": "8:00am",
           "endtime": "9:00am",
-          "description": "It's Pizza Time It's Pizza Time It's Pizza Time It's Pizza Time It's Pizza Time"
+          "description": "It's Pizza Time It's Pizza Time It's Pizza Time It's Pizza Time It's Pizza Time",
+          "id": "its-pizza-time-aug0302022"
         },
         {
           "title": "It's Pizza Time",
           "date": "Aug 3, 2022",
           "starttime": "8:00am",
           "endtime": "9:00am",
-          "description": "It's Pizza Time It's Pizza Time It's Pizza Time It's Pizza Time It's Pizza Time"
+          "description": "It's Pizza Time It's Pizza Time It's Pizza Time It's Pizza Time It's Pizza Time",
+          "id": "its-pizza-time-aug0302022"
         },
         {
           "title": "It's Pizza Time",
           "date": "Aug 3, 2022",
           "starttime": "8:00am",
           "endtime": "9:00am",
-          "description": "It's Pizza Time It's Pizza Time It's Pizza Time It's Pizza Time It's Pizza Time"
+          "description": "It's Pizza Time It's Pizza Time It's Pizza Time It's Pizza Time It's Pizza Time",
+          "id": "its-pizza-time-aug0302022"
         },
         {
           "title": "It's Pizza Time",
           "date": "Aug 3, 2022",
           "starttime": "8:00am",
           "endtime": "9:00am",
-          "description": "It's Pizza Time It's Pizza Time It's Pizza Time It's Pizza Time It's Pizza Time"
+          "description": "It's Pizza Time It's Pizza Time It's Pizza Time It's Pizza Time It's Pizza Time",
+          "id": "its-pizza-time-aug0302022"
         },
         {
           "title": "It's Pizza Time",
           "date": "Aug 3, 2022",
           "starttime": "8:00am",
           "endtime": "9:00am",
-          "description": "It's Pizza Time It's Pizza Time It's Pizza Time It's Pizza Time It's Pizza Time"
+          "description": "It's Pizza Time It's Pizza Time It's Pizza Time It's Pizza Time It's Pizza Time",
+          "id": "its-pizza-time-aug0302022"
         },
         {
           "title": "It's Pizza Time",
           "date": "Aug 3, 2022",
           "starttime": "8:00am",
           "endtime": "9:00am",
-          "description": "It's Pizza Time It's Pizza Time It's Pizza Time It's Pizza Time It's Pizza Time"
+          "description": "It's Pizza Time It's Pizza Time It's Pizza Time It's Pizza Time It's Pizza Time",
+          "id": "its-pizza-time-aug0302022"
         },
       ]
     }
